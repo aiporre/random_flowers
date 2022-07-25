9c98d018e76efe7e0b77b2b69c6b4cf78c0ff298
aiporre <ariel.iporre.rivas@gmail.com>
2022-07-25 21:12:41 +0200

    modification to make it work switch generate hadrcode imags

--- js/app.js
+++ js/app.js
@@ -5,20 +5,23 @@ images = ["img/background_color/complement_9BA0BC.png",
           "img/background_color/complement_B4AFB2.png",
           "img/background_color/complement_BBBEC8.png"];
 
-background_colors = [
-          "9BA0BC", 
-          "699CCD",
-          "88918D", 
-          "AFA7DD",
-          "B4AFB2",
-          "BBBEC8"];
+colors = [
+          "#9BA0BC", 
+          "#699CCD",
+          "#88918D", 
+          "#AFA7DD",
+          "#B4AFB2",
+          "#BBBEC8"];
 const btn = document.querySelector('#generate');
 const color = document.querySelector('.color');
+const flower = document.querySelector('.flower');
 
 btn.addEventListener('click', function() {
     // get a random number between 0 and lenght of colors 
     const random = Math.floor(Math.random() * colors.length);
     document.body.style.backgroundColor = colors[random];
+    flower.src = images[random]; 
     color.textContent = colors[random];
     console.log(random);
+    console.log(flower.src)
 });
