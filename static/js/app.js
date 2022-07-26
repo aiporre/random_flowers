5ccbd796ad9e633124eea665fc3212cf3f067612
aiporre <ariel.iporre.rivas@gmail.com>
2022-07-26 19:07:04 +0200

    renamed the css style javascript and images to an static folder

---
+++ static/js/app.js
@@ -0,0 +1,45 @@
+const URL="localhost";
+const PORT="8000";
+
+images = ["static/img/background_color/complement_9BA0BC.png", 
+          "static/img/background_color/complement_699CCD.png",
+          "static/img/background_color/complement_88918D.png", 
+          "static/img/background_color/complement_AFA7DD.png",
+          "static/img/background_color/complement_B4AFB2.png",
+          "static/img/background_color/complement_BBBEC8.png"];
+
+colors = [
+          "#9BA0BC", 
+          "#699CCD",
+          "#88918D", 
+          "#AFA7DD",
+          "#B4AFB2",
+          "#BBBEC8"];
+const btn = document.querySelector('#generate');
+const color = document.querySelector('.color');
+const flower = document.querySelector('.flower');
+
+// btn.addEventListener('click', function() {
+    // get a random number between 0 and lenght of colors 
+    // const random = Math.floor(Math.random() * colors.length);
+    // document.body.style.backgroundColor = colors[random];
+    // flower.src = images[random]; 
+    // color.textContent = colors[random];
+    // console.log(random);
+    // console.log(flower.src)
+// });
+
+btn.addEventListener('click', async function() {
+    // get a random number between 0 and lenght of colors A
+    let res = await fetch('generate');
+    console.log('the response is: ', res);
+    if (res.ok) {
+        let data = await res.json();
+        document.body.style.backgroundColor = data.color;
+        flower.src = data.img; 
+        color.textContent = data.color;
+        console.log(flower.src)
+    } else{
+        console.log('error while generating flower')
+    }
+});
--- static/js/generator.js
+++ static/js/generator.js
@@ -1,9 +1,13 @@
-images = ["img/background_color/complement_9BA0BC.png", 
-          "img/background_color/complement_699CCD.png",
-          "img/background_color/complement_88918D.png", 
-          "img/background_color/complement_AFA7DD.png",
-          "img/background_color/complement_B4AFB2.png",
-          "img/background_color/complement_BBBEC8.png"];
+URL="localhost"
+PORT="8000"
+
+localhost
+images = ["complement_9BA0BC.png", 
+          "complement_699CCD.png",
+          "complement_88918D.png", 
+          "complement_AFA7DD.png",
+          "complement_B4AFB2.png",
+          "complement_BBBEC8.png"];
 
 colors = [
           "#9BA0BC", 
@@ -20,7 +24,7 @@ btn.addEventListener('click', function() {
     // get a random number between 0 and lenght of colors 
     const random = Math.floor(Math.random() * colors.length);
     document.body.style.backgroundColor = colors[random];
-    flower.src = images[random]; 
+    flower.src = `${URL}:${PORT}/static/img/background_color/${images[random]}`; 
     color.textContent = colors[random];
     console.log(random);
     console.log(flower.src)
